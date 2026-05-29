93254832f05ab6b14105dd8db22b6bed8becbc7b
noctisvora <noctisvora@gmail.com>
2026-05-29 16:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const elements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
+document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("input[placeholder='Your Name']").value;
+    const budget = document.querySelector("input[placeholder='Budget']").value;
+    const project = document.querySelector("textarea").value;
+
+    const res = await fetch("http://localhost:3000/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            name,
+            email: "-",
+            budget,
+            project
+        })
+    });
+
+    alert(await res.text());
+});
+document.querySelector("form").addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    const email = document.getElementById("email").value;
+
+    const budget = document.getElementById("budget").value;
+
+    const project = document.getElementById("project").value;
+
+    const res = await fetch("http://localhost:3000/submit", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            name,
+            email,
+            budget,
+            project
+        })
+
+    });
+
+    alert(await res.text());
+
+});
